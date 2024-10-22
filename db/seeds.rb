338e6901fa9ba52b1d839cfa09ec6cf8b47ce677
claudiaamprimo <claudia.amprimo@gmail.com>
2024-10-22 19:48:33 +0200

    Implement product model, controller,routes and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(code: 'GR1', name: 'Green Tea', price: 3.11)
+Product.create(code: 'SR1', name: 'Strawberries', price: 5.00)
+Product.create(code: 'CF1', name: 'Coffee', price: 11.23)
