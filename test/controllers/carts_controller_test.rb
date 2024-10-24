65de19f6e19a7a608330b7e4d33b6e333c8ce92b
claudiaamprimo <claudia.amprimo@gmail.com>
2024-10-24 13:20:33 +0200

    add tests for carts controller and cart item model

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,4 +1,84 @@
 require "test_helper"
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    Product.create(code: 'GR1', name: 'Green Tea', price: 3.11)
+    Product.create(code: 'SR1', name: 'Strawberries', price: 5.00)
+    Product.create(code: 'CF1', name: 'Coffee', price: 11.23)
+  end
+
+  test "should add product and calculate total price with discount for green tea" do
+    post add_product_carts_url, params: { product_code: 'GR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'GR1' }
+    assert_response :success
+
+    get carts_url
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_in_delta 3.11, json['total_price'], 0.01
+  end
+
+  test "should apply discount for bulk strawberries purchase" do
+    post add_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+
+    get carts_url
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_in_delta 13.50, json['total_price'], 0.01
+  end
+
+  test "should apply discount for coffee when buying 3 or more" do
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+
+    get carts_url
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_in_delta 22.46, json['total_price'], 0.01
+  end
+
+  test "should calculate correctly total price with various products" do
+    post add_product_carts_url, params: { product_code: 'GR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+
+    get carts_url
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_in_delta 30.57, json['total_price'], 0.01
+  end
+
+  test "should remove product updating total price" do
+    post add_product_carts_url, params: { product_code: 'GR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+    post add_product_carts_url, params: { product_code: 'CF1' }
+    assert_response :success
+
+    delete remove_product_carts_url, params: { product_code: 'SR1' }
+    assert_response :success
+
+    get carts_url
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_in_delta 14.34, json['total_price'], 0.01
+  end
 end
--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class CartItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product_gr1 = Product.create(code: 'GR1', name: 'Green Tea', price: 3.11)
+    @product_sr1 = Product.create(code: 'SR1', name: 'Strawberries', price: 5.00)
+    @product_cf1 = Product.create(code: 'CF1', name: 'Coffee', price: 11.23)
+  end
+
+  test "total_price for GR1 with even 2" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_gr1, quantity: 2)
+    assert_in_delta 3.11, cart_item.total_price, 0.01
+  end
+
+  test "total_price for GR1 with odd quantity" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_gr1, quantity: 3)
+    assert_in_delta 6.22, cart_item.total_price, 0.01
+  end
+
+  test "total_price for SR1 with quantity less than 3" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_sr1, quantity: 2)
+    assert_in_delta 10.00, cart_item.total_price, 0.01
+  end
+
+  test "total_price for SR1 with quantity of 3 or more" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_sr1, quantity: 3)
+    assert_in_delta 13.50, cart_item.total_price, 0.01
+  end
+
+  test "total_price for CF1 with quantity less than 3" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_cf1, quantity: 2)
+    assert_in_delta 22.46, cart_item.total_price, 0.01
+  end
+
+  test "total_price for CF1 with quantity of 3 or more" do
+    cart = Cart.create
+    cart_item = cart.cart_items.create(product: @product_cf1, quantity: 3)
+    assert_in_delta 22.46, cart_item.total_price, 0.01
+  end
 end
